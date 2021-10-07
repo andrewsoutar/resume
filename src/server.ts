660c42bdcf2ff2a203f5e63f75464ba64ab9507d
Andrew Soutar <andrew@andrewsoutar.com>
2021-10-07 00:02:34 -0400

    Bump dependencies; switch to Express because Sapper has issues with Polka

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,13 @@
-import sirv from 'sirv';
-import polka from 'polka';
 import compression from 'compression';
+import express from 'express';
+import sirv from 'sirv';
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-                '/resume',
-	        compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+express().use('/resume',
+              compression({ threshold: 0 }),
+              sirv('static', { dev }),
+              sapper.middleware()
+).listen(PORT);
